70a3674a739b00e586bb161584e6d802960f7a1c
Copyes <fan_545697167@qq.com>
2017-05-31 10:20:47 +0800

    add config about dev and prod

---
+++ config/index.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+	dev: {
+		env: require('./dev-env.js'),
+		assetsRoot: path.resolve(__dirname, '../dist')
+	},
+
+	prod: {
+		env: require('./prod-env.js'),
+	}
+}
